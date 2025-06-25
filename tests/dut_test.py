5f7a80324fb43073e63b2bceef7534c2e23db036
Greeshmanreddy147 <mgreeshmanreddy147@gmail.com>
2025-06-25 16:11:48 +0530

    Update dut_test.py

--- tests/dut_test.py
+++ tests/dut_test.py
@@ -7,8 +7,8 @@ async def dut_test(dut):
     a = [0,0,1,1]
     b = [0,1,0,1]
     y = [0,1,1,0]
-for i in  range(4):
-    dut.a.value = a[i]
-    dut.b.value = b[i]
-    await Timer(1,"ns");
-    assert dut.y.value == y[i],f"error at iteration {i}"
+    for i in  range(4):
+        dut.a.value = a[i]
+        dut.b.value = b[i]
+        await Timer(1,"ns");
+        assert dut.y.value == y[i],f"error at iteration {i}"
